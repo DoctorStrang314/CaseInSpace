5e745c82f8bb49b12fdf771f91a4427e58823ecf
DoctorStrang314 <93518791+DoctorStrang314@users.noreply.github.com>
2022-05-12 01:21:21 +0300

    не хватает только настроек

--- i hate it/Assets/Scripts/MainMenu.cs
+++ i hate it/Assets/Scripts/MainMenu.cs
@@ -6,6 +6,7 @@ using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
     public GameObject ChooseLVL;
+   
     void Update()
     {
         if (Input.GetKey(KeyCode.Space)&& ChooseLVL.active==true)
@@ -13,10 +14,20 @@ public class MainMenu : MonoBehaviour
             
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
         }
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
     }
     public void ExitMenu()
     {
         SceneManager.LoadScene("Menu_scen");
-        
+        Time.timeScale = 1;
     }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+        print("by");
+    } 
 }
